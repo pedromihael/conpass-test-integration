0a131e2d2bf1c69362fbc64f9376dfd55a19cc13
pedro tec <pedromvpaes@gmail.com>
2020-09-22 09:02:23 -0400

    Updates

---
+++ precache-manifest.79ff0108552ad2c7b83f302ee6131b40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b840a525cadb532ab46ea3dda9b5f9ed",
+    "url": "/conpass-test-integration/index.html"
+  },
+  {
+    "revision": "e28295cedc30bfb68c2c",
+    "url": "/conpass-test-integration/static/js/2.6cfe351a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/conpass-test-integration/static/js/2.6cfe351a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a3b11a7515b1900214",
+    "url": "/conpass-test-integration/static/js/main.76a258d1.chunk.js"
+  },
+  {
+    "revision": "eac4b3066a7e1ab9233c",
+    "url": "/conpass-test-integration/static/js/runtime-main.4b37151a.js"
+  }
+]);
